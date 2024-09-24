12e6151a25b253ff87471a766812f95da50f254c
Uladzimir <yauhenij@gmail.com>
2024-09-24 18:59:16 +0300

    second step for self-propelled vehicles has been added

--- src/main/java/by/custom/utilcalculator/service/MessagesCreator.java
+++ src/main/java/by/custom/utilcalculator/service/MessagesCreator.java
@@ -39,7 +39,7 @@ public class MessagesCreator {
                 return getGreeting();
             }
             case PARTICULAR_TRANSPORT_TYPE -> {
-                return getTransportType();
+                return getTransportType(userProgress);
             }
             case WEIGHT -> {
                 return getTransportWeight(userProgress);
@@ -98,12 +98,44 @@ public class MessagesCreator {
                 Command.OTHER_COUNTRIES.getCommand(), " ", bundle.getString("answers.details.other"));
     }
 
-    public String getTransportType() {
-        return stringBuilderAppender(".", "\n", bundle.getString("questions.users.type.exceptM1"), "\n",
-                Command.N1_N3.getCommand(), " ", bundle.getString("answers.details.n1_n3"), "\n",
-                Command.M2_M3.getCommand(), " ", bundle.getString("answers.details.m2_m3"), "\n",
-                Command.TRUCK_UNITS.getCommand(), " ", bundle.getString("answers.details.truck_units"), "\n",
-                Command.TRAILERS_O4.getCommand(), " ", bundle.getString("answers.details.trailers04"));
+    public String getTransportType(final UserProgress userProgress) {
+        switch (userProgress.getGeneralTransportType()) {
+            case BUSES_AND_TRUCKS -> {
+                return stringBuilderAppender(".", "\n", bundle.getString("questions.users.type_of_vehicle"), "\n",
+                        Command.N1_N3.getCommand(), " ", bundle.getString("answers.details.n1_n3"), "\n",
+                        Command.M2_M3.getCommand(), " ", bundle.getString("answers.details.m2_m3"), "\n",
+                        Command.TRUCK_UNITS.getCommand(), " ", bundle.getString("answers.details.truck_units"), "\n",
+                        Command.TRAILERS_O4.getCommand(), " ", bundle.getString("answers.details.trailers04"));
+            }
+            case SELF_PROPELLED_VEHICLES -> {
+                return stringBuilderAppender(".", "\n", bundle.getString("questions.users.type_of_vehicle"), "\n",
+                        Command.HELP.getCommand(), " ", bundle.getString("answers.details.self_propelled_help"), "\n",
+
+                        Command.GRADERS.getCommand(), " ", bundle.getString("answers.details.graders"), "\n",
+                        Command.BULLDOZERS.getCommand(), " ", bundle.getString("answers.details.bulldozers"), "\n",
+                        Command.EXCAVATORS.getCommand(), " ", bundle.getString("answers.details.excavators"), "\n",
+                        Command.WHEEL_LOADERS.getCommand(), " ", bundle.getString("answers.details.wheel_loaders"), "\n",
+                        Command.TAMPING_MACHINES.getCommand(), " ", bundle.getString("answers.details.tamping_machines"), "\n",
+
+                        Command.FRONT_LOADERS.getCommand(), " ", bundle.getString("answers.details.front_loaders"), "\n",
+                        Command.WHEELED_CRANES.getCommand(), " ", bundle.getString("answers.details.wheeled_cranes"), "\n",
+                        Command.PIPELAYERS.getCommand(), " ", bundle.getString("answers.details.pipelayers"), "\n",
+                        Command.TRAILERS_OTHER.getCommand(), " ", bundle.getString("answers.details.trailers_other"), "\n",
+                        Command.ROAD_MAINTENANCE.getCommand(), " ", bundle.getString("answers.details.road_maintenance_vehicles"), "\n",
+
+                        Command.FORESTRY.getCommand(), " ", bundle.getString("answers.details.forestry_vehicles"), "\n",
+                        Command.FORWADERS.getCommand(), " ", bundle.getString("answers.details.forwaders"), "\n",
+                        Command.TIMBER_LOADERS.getCommand(), " ", bundle.getString("answers.details.timber_loaders"), "\n",
+                        Command.WHEELED_TRACTORS.getCommand(), " ", bundle.getString("answers.details.wheeled_tractors"), "\n",
+                        Command.CRAWLER_TRACTORS.getCommand(), " ", bundle.getString("answers.details.crawler_tractors"), "\n",
+
+                        Command.COMBINE_HARVESTERS.getCommand(), " ", bundle.getString("answers.details.combine_harvesters"), "\n",
+                        Command.FORAGE_HARVESTERS.getCommand(), " ", bundle.getString("answers.details.forage_harvesters"), "\n",
+                        Command.AGRICULTURAL_VEHICLES.getCommand(), " ", bundle.getString("answers.details.agricultural_vehicles"), "\n",
+                        Command.OFF_ROAD_DUMP_TRUCKS.getCommand(), " ", bundle.getString("answers.details.off_road_dump_trucks"));
+            }
+        }
+        return "unknown general transport type";
     }
 
     public String getTransportWeight(final UserProgress userProgress) {
@@ -223,15 +255,12 @@ public class MessagesCreator {
     }
 
     private void addSequenceGeneralTransportType(final UserProgress userProgress, final StringBuilder sb) {
-
         switch (userProgress.getGeneralTransportType()) {
             case null -> {
             }
             case M1 -> sb.append(bundle.getString("answers.summary.m1"));
-            case BUSES_AND_TRUCKS -> sb.append(bundle.getString("answers.summary.except_m1"));
-            case SELF_PROPELLED_VEHICLES -> {
-                sb.append(bundle.getString("answers.summary.self_propelled_vehicles"));
-            }
+            case BUSES_AND_TRUCKS -> sb.append(bundle.getString("answers.summary.buses_and_trucks"));
+            case SELF_PROPELLED_VEHICLES -> sb.append(bundle.getString("answers.summary.self_propelled_vehicles"));
         }
     }
 
