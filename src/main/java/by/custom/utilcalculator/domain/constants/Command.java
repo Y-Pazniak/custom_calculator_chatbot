fa2bd93da82578e5f81cc3ec77c755e75b8f6cdc
Uladzimir <yauhenij@gmail.com>
2024-10-11 14:25:05 +0300

    Families from command have been particulary removed

--- src/main/java/by/custom/utilcalculator/domain/constants/Command.java
+++ src/main/java/by/custom/utilcalculator/domain/constants/Command.java
@@ -197,6 +197,6 @@ public enum Command {
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
-        return null; //11122223333
+        return null;
     }
 }
